1174f4e01463bb912e354e706fe04900765b21bb
Benjamin Stern <ben.stern@yale.edu>
2021-04-10 11:00:30 -0400

    Updated R Script

--- R Script for HW 9.R
+++ R Script for HW 9.R
@@ -1,3 +1,6 @@
 # New R Script for HW 9
+install.packages("tidyverse")
+library(tidyverse)
+library("readxl")
 
-HW9DATA <- read_xlsx('C:/Users/14127/Downloads/Env Data Vis/Final Project Data/Newest EDV Project Dataset.xlsx') 
+HW9DATA <- read_excel('C:/Users/14127/Downloads/Env Data Vis/Final Project Data/Newest EDV Project Dataset.xlsx') 
